df293f8206322d71b9c8a1838557666db01a9c43
AGswasthikAG <swasthik.bsthr@gmail.com>
2024-07-24 18:15:36 +0530

    javascript programe

--- test/console.spec.js
+++
@@ -1,7 +0,0 @@
-const {exports,test}=require("@playwright/test")
-test("testing",async({page})=>
-{
-console.log("execution done")
-
-
-})
---
+++ test/javascript.js
@@ -0,0 +1,9 @@
+//Reverrse the word
+const name="Swasthik";
+var rev="";
+for(let i=name.length;i>=0;i--)
+{
+    rev+=name[i]
+}
+console.log(rev)
+
